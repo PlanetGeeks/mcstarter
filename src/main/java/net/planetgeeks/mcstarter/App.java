2278a6fdcaece08235a1dce590d7972cd850c40b
PlanetGeeks <PlanetGeekITA@gmail.com>
2014-12-13 00:14:13 +0100

    importing project

---
+++ src/main/java/net/planetgeeks/mcstarter/App.java
@@ -0,0 +1,14 @@
+package net.planetgeeks.mcstarter;
+
+import lombok.Getter;
+
+public class App 
+{
+	@Getter	
+	private String str;
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
