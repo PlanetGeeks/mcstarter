7ce4cccda8b610544b66f1046f646dc6a3058494
PlanetGeeks <PlanetGeekITA@gmail.com>
2014-12-13 00:49:01 +0100

    Test commit

--- src/main/java/net/planetgeeks/mcstarter/App.java
+++ src/main/java/net/planetgeeks/mcstarter/App.java
@@ -10,5 +10,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        
+        System.out.println("LOL");
     }
 }
